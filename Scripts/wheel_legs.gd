a2fda1c70c799884af34b2dc88334e867f82b0d9
Sofsh <bengoodall419@gmail.com>
2024-08-19 19:50:13 -0500

    Partial water implemented

--- Scripts/wheel_legs.gd
+++ Scripts/wheel_legs.gd
@@ -2,6 +2,9 @@ extends AttachablePart
 #variable "robot" can be used to refer to the player character
 
 const SPEED = 200.0
+@onready var water_check: Marker2D = $Marker2D
+
+var in_water_amount = 0
 
 func activate():
 	#In this case, jumping is handled in the physics process
@@ -12,7 +15,6 @@ func _physics_process(delta: float) -> void:
 	if not robot.is_on_floor():
 		robot.velocity += robot.get_gravity() * delta
 		
-
 	# Get the input direction and handle the movement/deceleration.
 	if robot.can_control:
 		var direction := Input.get_axis("ui_left", "ui_right")
@@ -25,5 +27,26 @@ func _physics_process(delta: float) -> void:
 	else:
 		robot.velocity.x = lerp(robot.velocity.x, 0.0, delta*4)
 		
+	
+	
+	if robot.is_on_floor() and robot.velocity.x != 0 and in_water_amount == 0:
+		robot.set_collision_mask_value(4, true)
+	else:
+		robot.set_collision_mask_value(4, false)
+	
+	
+	
 	robot.calculate_xy(delta)
 	robot.move_and_slide()
+
+
+
+
+func _on_water_checker_body_entered(body: Node2D) -> void:
+	in_water_amount = in_water_amount + 1
+
+
+func _on_water_checker_body_exited(body: Node2D) -> void:
+	in_water_amount = in_water_amount - 1
+	if in_water_amount < 0:
+		in_water_amount = 0
