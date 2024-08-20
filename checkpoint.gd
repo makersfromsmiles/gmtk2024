b1c7027f8895ca69a72fa3b3de9ad6d78454e9b4
/* JACK */ <makersfromsmiles@gmail.com>
2024-08-20 11:24:59 -0500

    Title Screen, Options, Failed Attempts
    auggh

--- checkpoint.gd
+++ checkpoint.gd
@@ -7,8 +7,10 @@ func process(delta):
 	else:
 		anim.play("fly", -1.0)
 
-
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.name == "Robot":
-		pass
+		var current_scene_file = get_tree().current_scene.scene_file_path
+		var next_level_number = current_scene_file.to_int() = 1
+		
+		var next_level_path = "res"
 		#Global.checkpoint_xy = position
